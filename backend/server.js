016e2bec854fe129aac351ba657d7763647084de
satyaki95 <www.satyaki95@gmail.com>
2026-05-12 21:08:52 +0530

    backend

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,29 @@ import express from "express";
 import dotenv from "dotenv";
 import todoRoutes from "./routes/todo.route.js";
 import { connectDB } from "./config/db.js";
+import cors from "cors";
+import path from "path";
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
 const app = express();
 
-// app.get("/", (req, res) => {
-//   res.send("Server is ready");
-// });
 app.use(express.json());
+// app.use(cors());
+
 app.use("/api/todos", todoRoutes);
 
-app.listen(5000, () => {
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
+app.listen(PORT, () => {
   connectDB();
-  console.log("Server is running on port http://localhost:5000");
+  console.log("Server started at http://localhost:5000");
 });
