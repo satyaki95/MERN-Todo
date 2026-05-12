b03074141a0c9de8e2305d72ab2a717648779801
satyaki95 <www.satyaki95@gmail.com>
2026-05-12 19:11:28 +0530

    backend

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
+import todoRoutes from "./routes/todo.route.js";
 import { connectDB } from "./config/db.js";
 
 dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Server is ready");
-});
+// app.get("/", (req, res) => {
+//   res.send("Server is ready");
+// });
+app.use(express.json());
+app.use("/api/todos", todoRoutes);
 
 app.listen(5000, () => {
   connectDB();
